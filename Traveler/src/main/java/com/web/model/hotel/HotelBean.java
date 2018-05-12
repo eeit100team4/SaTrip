2f4fb4a87115658982454807569925106a3b14c8
潘彣杰 <crazy_7711@yahoo.com.tw>
2018-05-12 22:38:13 +0800

    我的Bean髒髒= =

--- Traveler/src/main/java/com/web/model/hotel/HotelBean.java
+++ Traveler/src/main/java/com/web/model/hotel/HotelBean.java
@@ -3,24 +3,6 @@
 import java.io.Serializable;
 import java.sql.Blob;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-
-
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
-import org.springframework.web.multipart.MultipartFile;
-
-//本類別封裝單飯店資訊資料
-@Entity
-@Table(name = "Hotel")
 public class HotelBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -31,158 +13,13 @@ public class HotelBean implements Serializable {
 	private Blob coverImage;
 	private String file_name;
 	private String address;
-	private HotelRoomBean beanHotelRoom2;
-	private HotelRoomBean beanHotelRoom4;
+	
 	private Integer double_room_no;
 	private Integer quad_room_no;
 
-	private MultipartFile roomImage;
-
-	@Transient
-	@XmlTransient
-	public MultipartFile getRoomImage() {
-		return roomImage;
-	}
-
-	public void setRoomImage(MultipartFile roomImage) {
-		this.roomImage = roomImage;
-	}
-
-	
-	@JoinColumn(name = "double_room_no")
-	public HotelRoomBean getBeanHotelRoom2() {
-		return beanHotelRoom2;
-	}
-
-	public void setBeanHotelRoom2(HotelRoomBean beanHotelRoom2) {
-		this.beanHotelRoom2 = beanHotelRoom2;
-	}
-
-
-	@JoinColumn(name = "quad_room_no")
-	public HotelRoomBean getBeanHotelRoom4() {
-		return beanHotelRoom4;
-	}
-
-	public void setBeanHotelRoom(HotelRoomBean beanHotelRoom4) {
-		this.beanHotelRoom4 = beanHotelRoom4;
-	}
-
-
-
-	public HotelBean(Integer hotel_id, String country, String city, String hotel_name, Blob coverImage,
-			String file_name, String address, HotelRoomBean beanHotelRoom2, HotelRoomBean beanHotelRoom4) {
-		super();
-		this.hotel_id = hotel_id;
-		this.country = country;
-		this.city = city;
-		this.hotel_name = hotel_name;
-		this.coverImage = coverImage;
-		this.file_name = file_name;
-		this.address = address;
-		this.beanHotelRoom2 = beanHotelRoom2;
-		this.beanHotelRoom4 = beanHotelRoom4;
-	}
 	
 
-	public HotelBean() {
-		
-	}
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Integer getHotel_id() {
-		return hotel_id;
-	}
-
-	public void setHotel_id(Integer hotel_id) {
-		this.hotel_id = hotel_id;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getHotel_name() {
-		return hotel_name;
-	}
-
-	public void setHotel_name(String hotel_name) {
-		this.hotel_name = hotel_name;
-	}
-
-	public Blob getCoverImage() {
-		return coverImage;
-	}
-
-	public void setCoverImage(Blob coverImage) {
-		this.coverImage = coverImage;
-	}
-
-	public String getFile_name() {
-		return file_name;
-	}
-
-	public void setFile_name(String file_name) {
-		this.file_name = file_name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	public Integer getDouble_room_no() {
-		return double_room_no;
-	}
-
-	public void setDouble_room_no(Integer double_room_no) {
-		this.double_room_no = double_room_no;
-	}
-
-	public Integer getQuad_room_no() {
-		return quad_room_no;
-	}
-
-	public void setQuad_room_no(Integer quad_room_no) {
-		this.quad_room_no = quad_room_no;
-	}
-
-	@Override
-	public String toString() {
-		return "BeanHotel [hotel_id=" + hotel_id + ", country=" + country + ", city=" + city + ", hotel_name="
-				+ hotel_name + ", coverImage=" + coverImage + ", file_name=" + file_name + ", address=" + address
-				+ ", beanHotelRoom2=" + beanHotelRoom2 + ", beanHotelRoom4=" + beanHotelRoom4 + ", double_room_no="
-				+ double_room_no + ", quad_room_no=" + quad_room_no + ", roomImage=" + roomImage + ", getRoomImage()="
-				+ getRoomImage() + ", getBeanHotelRoom2()=" + getBeanHotelRoom2() + ", getBeanHotelRoom4()="
-				+ getBeanHotelRoom4() + ", getHotel_id()=" + getHotel_id() + ", getCountry()=" + getCountry()
-				+ ", getCity()=" + getCity() + ", getHotel_name()=" + getHotel_name() + ", getCoverImage()="
-				+ getCoverImage() + ", getFile_name()=" + getFile_name() + ", getAddress()=" + getAddress()
-				+ ", getDouble_room_no()=" + getDouble_room_no() + ", getQuad_room_no()=" + getQuad_room_no()
-				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
-				+ "]";
-	}
+	
 
 	
 
--- Traveler/src/main/java/com/web/model/hotel/MemberBean.java
+++ Traveler/src/main/java/com/web/model/hotel/MemberBean.java
@@ -4,188 +4,23 @@ import java.io.Serializable;
 import java.sql.Blob;
 import java.sql.Timestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "Member")
 public class MemberBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public MemberBean(Integer pkey, String account, String password, String first_name, String last_name,
-			String address, String phone, String email, String user_type, Timestamp registerTime, Double total_amt,
-			Blob memberImage, String fileName, Double unpaid_amt) {
-		super();
-		this.pkey = pkey;
-		this.account = account;
-		this.password = password;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.address = address;
-		this.phone = phone;
-		this.email = email;
-		this.user_type = user_type;
-		this.registerTime = registerTime;
-		this.total_amt = total_amt;
-		this.memberImage = memberImage;
-		this.fileName = fileName;
-		this.unpaid_amt = unpaid_amt;
-	}
+	private Integer pkey;
+	private String account;
+	private String password;
+	private String first_name;
+	private String last_name;
+	private String address;
+	private String phone;
+	private String email;
+	private String user_type;
+	private Timestamp registerTime;
+	private Double total_amt;
+	private Blob memberImage;
+	private String fileName;
+	private Double unpaid_amt;
 
-	public MemberBean(String account, String password, String first_name, String last_name, String address,
-			String phone, String email) {
-		super();
-		this.account = account;
-		this.password = password;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.address = address;
-		this.phone = phone;
-		this.email = email;
-	}
-
-	public MemberBean() {
-		super();
-	}
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	// @Column(name="seqNo")
-	public Integer getPkey() {
-		return pkey;
-	}
-
-	Integer pkey;
-	String account;
-	String password;
-	String first_name;
-	String last_name;
-	String address;
-	String phone;
-	String email;
-	String user_type;
-	Timestamp registerTime;
-	Double total_amt;
-	Blob memberImage;
-	String fileName;
-	Double unpaid_amt;
-
-	public void setPkey(Integer pkey) {
-		this.pkey = pkey;
-	}
-
-	@Column(name = "account")
-	public String getAccount() {
-		return account;
-	}
-
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	@Column(name="password")
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Column(name = "fileName")
-	public String getFirst_name() {
-		return first_name;
-	}
-	@Column(name="first_name")
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getLast_name() {
-		return last_name;
-	}
-	@Column(name="last_name")
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-	@Column(name="address")
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getUser_type() {
-		return user_type;
-	}
-
-	public void setUser_type(String user_type) {
-		this.user_type = user_type;
-	}
-	@Column(name="Register_date")
-	public Timestamp getRegisterTime() {
-		return registerTime;
-	}
-	@Column(name="address")
-	public void setRegisterTime(Timestamp registerTime) {
-		this.registerTime = registerTime;
-	}
-	@Column(columnDefinition="decimal(12, 1")
-	public Double getTotal_amt() {
-		return total_amt;
-	}
-
-	public void setTotal_amt(Double total_amt) {
-		this.total_amt = total_amt;
-	}
-
-	public Blob getMemberImage() {
-		return memberImage;
-	}
-
-	public void setMemberImage(Blob memberImage) {
-		this.memberImage = memberImage;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-	@Column(columnDefinition="decimal(12, 1")
-	public Double getUnpaid_amt() {
-		return unpaid_amt;
-	}
-
-	public void setUnpaid_amt(Double unpaid_amt) {
-		this.unpaid_amt = unpaid_amt;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 
 }
