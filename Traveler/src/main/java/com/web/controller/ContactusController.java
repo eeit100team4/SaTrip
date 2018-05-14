6a94e635ebecc2e11afed828cd74bc8ecbdb2266
黃美凌 <III@192.168.31.76>
2018-05-14 17:12:47 +0800

    修改意見表頁面

--- Traveler/src/main/java/com/web/controller/ContactusController.java
+++ Traveler/src/main/java/com/web/controller/ContactusController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,6 +26,9 @@ public class ContactusController {
 
 	@Autowired
 	ContactusService contactusService;
+	@Autowired
+	SessionFactory factory;
+	
 	
 	@RequestMapping("contactus/contactusopinion")
 	public String list(Model model) {
@@ -37,19 +44,30 @@ public class ContactusController {
 		map.put("contactusBean",contactus);
 	}
 	
+	
 	@RequestMapping(value="contactus/InsertOpinionSuccess",method=RequestMethod.GET)
-	public String getAddNewContactusForm(Model model) {
+	public String getAddNewContactusForm(@ModelAttribute("contactusBean")ContactusBean contactus2,Model model) {
 		System.out.println("enter");
-		ContactusBean contactus = new ContactusBean();
-		model.addAttribute("contactusBean",contactus);
+//		ContactusBean contactus = new ContactusBean();
+		
+		model.addAttribute("contactusBean",contactus2);
 		return "contactus/InsertOpinionSuccess";
 	}
 	@RequestMapping(value="contactus/InsertOpinionSuccess",method=RequestMethod.POST)
-	public String processAddNewContactusForm(@ModelAttribute("contactusBean")ContactusBean contactus,
-			BindingResult result, HttpServletRequest request) {
+	public String processAddNewContactusForm(@ModelAttribute("contactusBean")ContactusBean contactus2,
+			BindingResult result, HttpSession session) {
 		System.out.println("ENTER POST");
-		System.out.println(contactus);
-		contactusService.addCustomerOpinion(contactus);
+		System.out.println(contactus2);
+//		session.setAttribute("abc", contactus);
+		contactusService.addCustomerOpinion(contactus2);
+		session.setAttribute("aa", contactus2);
 		return "redirect:/contactus/InsertOpinionSuccess";
 	}
+	@RequestMapping("/update/feedbackopinion")
+	public String update(@ModelAttribute("contactusBean")ContactusBean contactus2
+			) {
+		contactusService.update(contactus2);
+		return "redirect:/feedbackopinion";
+	}
+	
 }
