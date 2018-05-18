2340197b88e98b57fc910b152b2577213b90b139
江姿瑩 <h6300kimo@gmail.com>
2018-05-18 17:37:03 +0800

    member update

---
+++ Traveler/src/main/java/com/web/controller/MemberController.java
@@ -0,0 +1,344 @@
+﻿package com.web.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.web.Utils.DateUtils;
+import com.web.model.member.MemberBean;
+import com.web.service.member.GlobalService;
+import com.web.service.member.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired
+	MemberService memberService;
+
+	@Autowired
+	ServletContext context;
+	
+	@RequestMapping("/changePwd")
+	public String changePwd(HttpServletRequest request, HttpServletResponse response, Model model) {
+		HttpSession session = request.getSession();
+		MemberBean mb = (MemberBean) session.getAttribute("LoginOK");
+		model.addAttribute("memberId", mb.getMemberId());
+		return "changePwd";
+	}
+
+	@RequestMapping("/changePwd.do")
+	public String changePwd(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("doChangePwd");
+
+		String succPage = "updateSuccess";
+		String errPage = "changePwd";
+
+		HttpSession session = request.getSession();
+		MemberBean mb = (MemberBean) session.getAttribute("LoginOK");
+
+		if (mb == null) {
+			return "login";  //如果未登入，mb=null時，就導頁至登入畫面
+		}
+		request.setCharacterEncoding("UTF-8");
+		// 準備存放錯誤訊息的 List 物件
+		Map<String, String> errorMsg = new HashMap<>();
+		request.setAttribute("errorMsg", errorMsg);
+		// 1. 讀取使用者輸入資料
+		System.out.println("1. 讀取使用者輸入資料");
+		String oldPassword = request.getParameter("oldPassword");
+		String password = request.getParameter("password");
+		String chkPassword = request.getParameter("chkPassword");
+		System.out.println("oldPassword=" + oldPassword + " ,password=" + password + " ,chkPassword=" + chkPassword);
+		// 3. 檢查使用者必輸入資料
+		System.out.println("3. 檢查使用者必輸入資料");
+		// 新增時的驗證
+
+		if (oldPassword == null || oldPassword.trim().length() == 0) {
+			errorMsg.put("oldPassword", "舊密碼欄必須輸入");
+		} else {
+			String encodePassword = GlobalService.encryptString(oldPassword);
+			String sessionEncodePwd = GlobalService.encryptString(mb.getPassword());
+			if (encodePassword.equals(sessionEncodePwd)) {
+				errorMsg.put("oldPassword", "舊密碼輸入錯誤");
+			} else {
+
+			}
+		}
+		if (password == null || password.trim().length() == 0) {
+			errorMsg.put("password", "密碼欄必須輸入");
+		}
+		if (chkPassword == null || chkPassword.trim().length() == 0) {
+			errorMsg.put("chkPassword", "確認密碼欄必須輸入");
+		} else {
+			if (!chkPassword.equals(password)) {
+				errorMsg.put("chkPassword", "確認密碼欄必須和密碼欄位一致");
+			}
+		}
+		if (!errorMsg.isEmpty()) {
+			return errPage;
+		}
+		try {
+			String newEncodePassword = GlobalService.encryptString(password);
+			//memberService.changePwd(mb.getMemberId(), newEncodePassword);
+			mb.setPassword(newEncodePassword);
+			memberService.updateMember(mb);
+		} catch (Exception e) {
+			errorMsg.put("memberId", "儲存資料時發生錯誤，請檢查，例外=" + e.getMessage());
+			e.printStackTrace();
+			return errPage;
+		}
+		request.setAttribute("chineseLastName", mb.getChineseLastName());
+		request.setAttribute("chineseFirstName", mb.getChineseFirstName());
+		
+		return succPage;
+	}
+
+	@RequestMapping("/updateMember")
+	public String updateMember(HttpServletRequest request, HttpServletResponse response, Model model) {
+		HttpSession session = request.getSession();
+		MemberBean mb = (MemberBean) session.getAttribute("LoginOK");
+		model.addAttribute("welcomeNm", mb.getMemberId());
+		model.addAttribute("member", mb);
+		model.addAttribute("function", "update");
+		return "saveMember";
+	}
+
+	@RequestMapping("/members")
+	public String list(Model model) {
+		List<MemberBean> list = memberService.getAllMembers();
+		model.addAttribute("members", list);
+		return "members";
+	}
+
+	@RequestMapping("/member")
+	public String getMemberById(@RequestParam("memberId") String memberId, Model model) {
+		model.addAttribute("member", memberService.getMemberById(memberId));
+		return "member";
+	}
+
+	@RequestMapping("/register")
+	public String register(Model model) {
+		System.out.println("register");
+		model.addAttribute("function", "add");
+		return "saveMember";
+	}
+
+	@RequestMapping("/saveMember.do")
+	public String doSaveMember(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("doSaveMember");
+
+		String succPage = "success";
+		String errPage = "saveMember";
+
+		request.setCharacterEncoding("UTF-8");
+		// 準備存放錯誤訊息的 List 物件
+		Map<String, String> errorMsg = new HashMap<>();
+		request.setAttribute("errorMsg", errorMsg);
+		// 1. 讀取使用者輸入資料
+		System.out.println("1. 讀取使用者輸入資料");
+		String memberId = request.getParameter("memberId");
+		String gender = request.getParameter("gender");
+		String password = request.getParameter("password");
+		String chkPassword = request.getParameter("chkPassword");
+		String chineseLastName = request.getParameter("chineseLastName");
+		String chineseFirstName = request.getParameter("chineseFirstName");
+		String englishLastName = request.getParameter("englishLastName");
+		String englishFirstName = request.getParameter("englishFirstName");
+		String birthday = request.getParameter("birthday");
+		String email = request.getParameter("email");
+		String mobile = request.getParameter("mobile");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String address = request.getParameter("address");
+		String passportNumber = request.getParameter("passportNumber");
+		String function = request.getParameter("function");// update or add
+		System.out.println("function="+function+" memberId=" + memberId + " ,gender=" + gender + " ,password=" + password + " ,chkPassword="
+				+ chkPassword + " ,chineseLastName=" + chineseLastName + " ,chineseFirstName=" + chineseFirstName
+				+ " ,englishLastName=" + englishLastName + " ,englishFirstName=" + englishFirstName + " ,birthday="
+				+ birthday + " ,email=" + email + " ,mobile=" + mobile + " ,phoneNumber=" + phoneNumber + " ,address="
+				+ address + " ,passportNumber=" + passportNumber);
+		// 2. 進行必要的資料轉換
+		System.out.println("2. 進行必要的資料轉換");
+		
+		java.sql.Date bdate = null;
+		try {
+			if (birthday != null && birthday.trim().length() > 0) {
+				Date bd = DateUtils.getDateByString(birthday);
+				if (bd!=null) {
+					bdate = new java.sql.Date(bd.getTime());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 3. 檢查使用者必輸入資料
+		System.out.println("3. 檢查使用者必輸入資料");
+		if ("add".equals(function)) {
+			// 新增時的驗證
+			if (memberId == null || memberId.trim().length() == 0) {
+				errorMsg.put("memberId", "帳號欄必須輸入身份證");
+			}
+			if (password == null || password.trim().length() == 0) {
+				errorMsg.put("password", "密碼欄必須輸入");
+			}
+			if (chkPassword == null || chkPassword.trim().length() == 0) {
+				errorMsg.put("chkPassword", "確認密碼欄必須輸入");
+			} else {
+				if (!chkPassword.equals(password)) {
+					errorMsg.put("chkPassword", "確認密碼欄必須和密碼欄位一致");
+				}
+			}
+		} else {
+			// 更新時的驗證
+			succPage = "updateSuccess";
+			errPage = "saveMember";
+		}
+		// 共用時的驗證
+		if (gender == null || gender.trim().length() == 0) {
+			errorMsg.put("gender", "性別欄必須輸入");
+		}
+
+		if (birthday == null || birthday.trim().length() == 0) {
+			errorMsg.put("birthday", "生日欄必須輸入");
+		}
+		if ((chineseLastName == null || chineseLastName.trim().length() == 0)
+				|| (chineseFirstName == null || chineseFirstName.trim().length() == 0)) {
+			errorMsg.put("name", "中文姓名欄必須輸入");
+		}
+
+		if (email == null || email.trim().length() == 0) {
+			errorMsg.put("email", "email欄必須輸入");
+		}
+		if (mobile == null || mobile.trim().length() == 0) {
+			errorMsg.put("mobile", "手機欄必須輸入");
+		}
+		if (!errorMsg.isEmpty()) {
+			return errPage;
+		}
+		// 4. 進行 Business Logic 運算
+		System.out.println("4. 進行 Business Logic 運算");
+		if ("add".equals(function)) {
+			// 新增時的邏輯
+			if (memberService.idExists(memberId)) {
+				errorMsg.put("memberId", "該身份證帳號 (" + memberId + ") 已經存在，請勿重複申請");
+			} else {
+				// 加密後的password為encodePassword
+				String encodePassword = GlobalService.encryptString(password);
+				try {
+					MemberBean member = new MemberBean(memberId, encodePassword, gender, chineseLastName,
+							chineseFirstName, englishLastName, englishFirstName, bdate, email, mobile, phoneNumber,
+							address, passportNumber);
+					memberService.addMember(member);
+				} catch (Exception e) {
+					errorMsg.put("memberId", "儲存資料時發生錯誤，請檢查，例外=" + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		} else {
+			// 更新時的邏輯
+			try {
+				HttpSession session = request.getSession();
+				MemberBean mb = (MemberBean) session.getAttribute("LoginOK");// mb
+				// MemberBean member = new MemberBean(mb.getMemberId(), mb.getPassword(),
+				// gender, chineseLastName, chineseFirstName, englishLastName,
+				// englishFirstName, bdate, email, mobile, phoneNumber, address,
+				// passportNumber);
+				mb.setGender(gender);
+				mb.setGender(gender);
+				mb.setChineseLastName(chineseLastName);
+				mb.setChineseFirstName(chineseFirstName);
+				mb.setEnglishLastName(englishLastName);
+				mb.setEnglishFirstName(englishFirstName);
+				mb.setBirthday(bdate);
+				mb.setEmail(email);
+				mb.setMobile(mobile);
+				mb.setPhoneNumber(phoneNumber);
+				mb.setAddress(address);
+				mb.setPassportNumber(passportNumber);
+
+				memberService.updateMember(mb);
+			} catch (Exception e) {
+				errorMsg.put("memberId", "儲存資料時發生錯誤，請檢查，例外=" + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+
+		// 5.依照 Business Logic 運算結果來挑選適當的畫面
+		System.out.println("5.依照 Business Logic 運算結果來挑選適當的畫面");
+		request.setAttribute("memberId", memberId);
+		System.out.println("errorMsg.isEmpty()="+errorMsg.isEmpty()
+		+" go page->"+(errorMsg.isEmpty()?succPage:errPage));
+		if (errorMsg.isEmpty()) {
+			return succPage;
+		} else {
+			return errPage;
+		}
+	}
+	
+	 @RequestMapping(value = "/getPicture/{memberId}", method = RequestMethod.GET)
+	 public ResponseEntity<byte[]> getPicture(HttpServletResponse resp,
+	 @PathVariable String memberId) {
+	 MemberBean bean = memberService.getMemberById(memberId);
+	 HttpHeaders headers = new HttpHeaders();
+	 Blob blob = bean.getMemberImage();
+	 int len = 0;
+	 byte[] media = null;
+	 if (blob != null) {
+	 try {
+	 len = (int) blob.length();
+	 media = blob.getBytes(1, len);
+	 } catch (SQLException e) {
+	 throw new RuntimeException("ProductController的getPicture()發生SQLException: " +
+	 e.getMessage());
+	 }
+	 } else {
+	 InputStream is =
+	 context.getResourceAsStream("/resources/images/bigHead.png");
+	 ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	 byte[] b = new byte[8192];
+	 try {
+	 while ((len = is.read(b)) != -1) {
+	 baos.write(b, 0, len);
+	 }
+	 } catch (Exception e) {
+	 throw new RuntimeException("ProductController的getPicture()發生IOException: "
+	 + e.getMessage());
+	 }
+	 media = baos.toByteArray();
+	 }
+	 headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+	 headers.setContentType(MediaType.IMAGE_JPEG);
+	 headers.setContentType(MediaType.IMAGE_GIF);
+	 ResponseEntity<byte[]> responseEntity =
+	 new ResponseEntity<>(media, headers, HttpStatus.OK);
+	 return responseEntity;
+	 }
+
+}
