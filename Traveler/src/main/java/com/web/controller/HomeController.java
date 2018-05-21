2e19219b8e06d5bd5c47c25fbb7d7f1469a1cdb8
黃光賢 <ddia@EEIT>
2018-05-22 00:11:23 +0800

    調整homeController後台路徑

--- Traveler/src/main/java/com/web/controller/HomeController.java
+++ Traveler/src/main/java/com/web/controller/HomeController.java
@@ -12,9 +12,9 @@ public class HomeController {
 	 return "index";
 	 }
 	 
-	 @RequestMapping({"/backStage"})
+	 @RequestMapping({"/backStageForm"})
 	 public String backStage() {
-	 return "backstage";
+	 return "backStageForm";
 	 }
 	 
 }
